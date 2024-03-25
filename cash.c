eb16cddaa13342b662303aff9651e3b4f30f2613
Misbah Haque <misbah.haque@marketleader.com>
2024-03-26 02:20:09 +0500

    added c variable

--- cash.c
+++ cash.c
@@ -6,6 +6,11 @@ int main()
 
 int i=0;
 printf("every code begins with Hello World how are you ?.....%i\n",i);
-int j=0;
+int j=200;
+int c=90;
+
+printf("second step is performing........ addition........... %i\n",j);
+
+
 
 }
